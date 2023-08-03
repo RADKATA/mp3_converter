2baf67d042ce8af87b93d6821fe458cb8382002b
Radoslav Petkov <54353388+RADKATA@users.noreply.github.com>
2023-08-03 22:31:31 +0100

    Update mp3_app.py

--- mp3_app/mp3_app.py
+++ mp3_app/mp3_app.py
@@ -1,32 +1,33 @@
 import tkinter as tk
 from tkinter.scrolledtext import ScrolledText
-from tkinter import ttk
+from tkinter import  ttk
 from pytube import YouTube
 
 
 class YouTubeDownloaderApp:
     def __init__(self, root):
         self.root = root
-        self.root.title("MP3 player Converter")
+        self.root.title("MP3 player conventon")
         self.root.resizable(False, False)
 
         self.create_widgets()
         self.apply_styles()
 
     def create_widgets(self):
-        label = tk.Label(self.root, text="YouTube Downloader", font=("Helvetica", 24, "bold"), background="#212a54")
+
+        label = tk.Label(self.root, text="YouTube Downloader", font=("Helvetica", 24, "bold"),background="#212a54")
         label.pack(pady=5)
 
         file_frame = tk.Frame(self.root)
         file_frame.pack(padx=20, pady=5, fill="x")
 
-        link_text = ScrolledText(self.root, wrap=tk.WORD, height=10, width=80, font=("Helvetica", 10),
-                                 background="#494b54")
-        link_text.pack(padx=20, pady=(0, 5))
-        self.link_text = link_text
-        self.link_text.config(state="disabled")
 
-        self.link_entry = tk.Entry(self.root, width=80, bg="#7f828f")
+
+        link_text = ScrolledText(self.root, wrap=tk.WORD, height=10, width=80, font=("Helvetica", 10))
+        link_text.pack(padx=20, pady=(0, 5))
+        self.link_text = link_text  # Store link_text as a member variable
+
+        self.link_entry = tk.Entry(self.root, width=80)
         self.link_entry.pack(padx=20, pady=(0, 5))
 
         add_button = ttk.Button(self.root, text="Add Link", command=self.add_link)
@@ -38,10 +39,9 @@ class YouTubeDownloaderApp:
         download_button = ttk.Button(self.root, text="Download Videos", command=self.download_videos)
         download_button.pack(padx=20, pady=(0, 5))
 
-        self.output_text = ScrolledText(self.root, wrap=tk.WORD, height=10, width=40, font=("Ariel", 10),
-                                        background="#494b54")
+        self.output_text = ScrolledText(self.root, wrap=tk.WORD, height=10, width=40, font=("Ariel", 10))
         self.output_text.pack(padx=20, pady=(0, 10))
-        self.output_text.config(state="disabled")
+
 
     def load_links(self, file_path):
         with open(file_path, "r") as file:
@@ -79,22 +79,21 @@ class YouTubeDownloaderApp:
             yd.download('./MP3 player')
             self.output_text.insert(tk.END, f"Downloaded: {yt.title}\n")
 
-        self.output_text.update()
+        self.output_text.update()  # Update output_text widget once
 
     def apply_styles(self):
-        self.root.configure(bg="#212a54")
+        self.root.configure(bg="#212a54")  # Cherry Red background color
 
         style = ttk.Style()
-        style.configure("TButton", padding=5, relief="flat", background="black", foreground="black")
-        style.map("TButton", background=[("active", "#333333")])
+        style.configure("TButton", padding=5, relief="flat", background="black", foreground="black")  # Change button colors here
+        style.map("TButton", background=[("active", "#333333")])  # Change active button color here
 
         # Adding hover effect
         style.map("TButton",
                   background=[("active", "#333333"),
                               ("!active", "black")])
-
-
 if __name__ == "__main__":
     root = tk.Tk()
     app = YouTubeDownloaderApp(root)
     root.mainloop()
+
